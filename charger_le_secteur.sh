a237a10ed7885fc6cbb2e4cfb4ca2bbf1f4fb987
loens_g <gregoire.loens@epitech.eu>
2017-10-24 20:48:54 +0200

    IL EST VIVANT

--- charger_le_secteur.sh
+++ charger_le_secteur.sh
@@ -1,3 +1,4 @@
-nasm -f bin -o bootsect sources/sector.asm
-cat bootsect /dev/zero | dd of=floppyA bs=512 count=28
+nasm -i sources/ -f bin -o bootsect sources/sector_protege.asm
+nasm -f bin -o kernel sources/mini_kernel.asm
+cat bootsect kernel /dev/zero | dd of=floppyA bs=512 count=2880
 qemu-system-x86_64 "floppyA"
